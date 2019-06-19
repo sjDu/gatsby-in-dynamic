4f112f3457282de574c507e6eb4045fc4e188025
sjDu <shengjhandu.gorillatec@gmail.com>
2019-06-19 13:13:11 +0800

    Modify some info

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'JAMstack Hackathon Starter',
+    title: 'Play JS yet another blog by sjdu',
     description: `Kick off your next, great Gatsby app.`,
     author: `@swyx`,
   },
